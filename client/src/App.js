c8741fc5d28fafd98729cc5172e3e09b98850070
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-05-04 11:53:48 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/styles.css";
+import './components/MainPage'
+import { MainPage } from "./components/MainPage";
+
+function App() {
+  return (
+    
+      <div className="App">
+    <MainPage/>
+
+      </div>
+  
+  );
+}
+
+export default App;
